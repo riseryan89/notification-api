4489dd7e12ddefdd74c8b991c209775ff055ef4d
ryan <rise.ryan.lee@gmail.com>
2020-12-26 23:54:54 +0900

    Login Series 5

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -57,7 +57,19 @@ async def register(sns_type: SnsType, reg_info: models.UserRegister, session: Se
 
 @router.post("/login/{sns_type}", status_code=200)
 async def login(sns_type: SnsType, user_info: models.UserRegister):
-    ...
+    if sns_type == SnsType.email:
+        is_exist = await is_email_exist(user_info.email)
+        if not user_info.email or not user_info.pw:
+            return JSONResponse(status_code=400, content=dict(msg="Email and PW must be provided'"))
+        if not is_exist:
+            return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
+        user = Users.get(email=user_info.email)
+        is_verified = bcrypt.checkpw(user_info.pw.encode("utf-8"), user.pw.encode("utf-8"))
+        if not is_verified:
+            return JSONResponse(status_code=400, content=dict(msg="NO_MATCH_USER"))
+        token = dict(Authorization=f"Bearer {create_access_token(data=UserToken.from_orm(user).dict(exclude={'pw', 'marketing_agree'}),)}")
+        return token
+    return JSONResponse(status_code=400, content=dict(msg="NOT_SUPPORTED"))
 
 
 async def is_email_exist(email: str):
